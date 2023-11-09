e23bef305233e39746e5987421affed70855db4e
Jessica-ovabor <ovaborjessica85@gmail.com>
2023-11-09 22:09:00 +0100

    windows-docKer-config

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,18 +1,25 @@
 #!/bin/bash
 
-# Activate the virtual environment
-source "$(pipenv --venv)/bin/activate"
+# Check if running on Windows
+if [ -n "$WINDIR" ]; then
+    # Activate the virtual environment in PowerShell
+    . "$(pipenv --venv)/Scripts/Activate.ps1"
+else
+    # Activate the virtual environment on Unix-like systems
+    source "$(pipenv --venv)/bin/activate"
+fi
 
+# Rest of your script
 # Wait for Redis to be ready
 /wait-for-it.sh -t 30 my_redis_container:6379 -- echo "Redis is ready"
 
-# Wait for the database to be ready (adjust as needed)
+# Wait for the database to be ready
 /wait-for-it.sh -t 30 my_postgres_container:5432 -- echo "PostgreSQL is ready"
 
 # Run database initialization (cli.py init-db)
 pipenv run python ./turbomed/cli.py init-db
 
-pipenv run python ./turbomed/cli.py create-admin victor.i@turbham.com --password=vannessa1#
+pipenv run python ./turbomed/cli.py create-admin jessica.o@turbham.com --password=password123
 
 # Start the Flask app
 pipenv run flask --app turbomed/app run --debug --host 0.0.0.0
